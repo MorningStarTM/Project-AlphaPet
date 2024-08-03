c12df41cb3386d5fa0f4bebb254839b033116e10
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-03 15:18:27 +0530

    bullet class created

---
+++ astro-avengers/bullet.py
@@ -0,0 +1,15 @@
+from .const import *
+
+class Bullet:
+    def __init__(self, x, y):
+        self.image = BULLET_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed = 10
+    
+    def update(self):
+        self.rect.y -= self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
