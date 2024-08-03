3df8b3eeb4317dfe5ae572e793cbcbea24212e30
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-03 16:44:11 +0530

    ammo component created

--- astro-avengers/gla.py
+++ astro-avengers/gla.py
@@ -41,3 +41,22 @@ class Life:
     def reset_position(self):
         self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
         self.rect.y = random.randint(-100, -40)
+
+
+class Ammunition:
+    def __init__(self):
+        self.image = AMMUNITION_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
+        self.speed = 2
+    
+    def update(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def reset_position(self):
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
