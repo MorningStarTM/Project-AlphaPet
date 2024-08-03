3c3149c7bb436e39c51349fef1fc5798087e0897
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-03 16:43:17 +0530

    shield component created

--- astro-avengers/gla.py
+++ astro-avengers/gla.py
@@ -1,1 +1,24 @@
 #Gaurd Life Ammunation
+from const import *
+import random
+
+
+class Shield:
+    def __init__(self):
+        self.image = SHIELD_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
+        self.speed = 2
+    
+    def update(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def reset_position(self):
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
+
+
