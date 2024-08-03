0421d512d1856906b8659588b88f51442606a4b0
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-03 16:43:52 +0530

    life component created

--- astro-avengers/gla.py
+++ astro-avengers/gla.py
@@ -22,3 +22,22 @@ class Shield:
         self.rect.y = random.randint(-100, -40)
 
 
+
+
+class Life:
+    def __init__(self):
+        self.image = LIFE_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
+        self.speed = 2
+    
+    def update(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def reset_position(self):
+        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
