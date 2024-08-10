fc289d873bb160d2be2d4d22580e6cc0b412b887
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-10 16:06:31 +0530

    enemy bullet created

--- astro-avengers/bullet.py
+++ astro-avengers/bullet.py
@@ -13,3 +13,19 @@ class Bullet:
     
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+
+
+class EnemyBullet:
+    def __init__(self, x, y):
+        self.image = BULLET_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed = 10
+    
+    def update(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
