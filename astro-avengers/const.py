3993256d321f1326bdbd4ff79c6565656198f380
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-11 11:52:15 +0530

    explosion images added

--- astro-avengers/const.py
+++ astro-avengers/const.py
@@ -1,11 +1,22 @@
 import pygame
-
+import os
 
 PLAYER_IMAGE = pygame.image.load("assets\\Player_1\\ship02P0000.png")
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+ENEMY_FLIGHT_IMAGE = pygame.image.load("assets\\enemy_1\\Turret09.png")
+SCREEN_WIDTH = 1100
+SCREEN_HEIGHT = 1000
 BULLET_IMAGE = pygame.image.load("assets\\bullet\\bullet.png")
 MISSILE_IMAGE = pygame.image.load("assets\\bullet\\P05.png")
 SHIELD_IMAGE = pygame.image.load("assets\\GLA\\shield.png")
 LIFE_IMAGE = pygame.image.load("assets\\GLA\\ammunition.png")
 AMMUNITION_IMAGE = pygame.image.load("assets\\GLA\\life.png")
+
+
+HUD_WIDTH = 200  # Width of the HUD section
+
+# Colors
+BLACK = (0, 0, 0)
+LIGHT_GRAY = (220, 220, 220)
+
+
+LIST_OF_EXPLOSION_IMAGE = [pygame.image.load(i) for i in os.listdir("astro-avengers\\assets\\explosion")]
