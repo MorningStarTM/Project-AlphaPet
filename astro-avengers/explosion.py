82af9f4d24b0b53612ffa4277aa02d0846273bf4
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-11 11:53:14 +0530

    explosion class updated and draw function created

--- astro-avengers/explosion.py
+++ astro-avengers/explosion.py
@@ -10,3 +10,20 @@ class Explosion:
         self.animation_speed = 5  # Adjust for how fast the explosion animates
         self.counter = 0
         self.done = False
+
+    def update(self):
+        """ Update the explosion animation """
+        if not self.done:
+            self.counter += 1
+            if self.counter >= self.animation_speed:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.done = True
+                else:
+                    self.image = self.images[self.index]
+
+    def draw(self, screen):
+        """ Draw the explosion on the screen """
+        if not self.done:
+            screen.blit(self.image, self.rect)
