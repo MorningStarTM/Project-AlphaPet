649471e66bf26a115d91531af351d30a85bff001
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-12 21:10:00 +0530

    decepticon flight created

---
+++ astro-avengers/decepticons.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+import math
+from const import *  # Assuming these are in const.py
+from explosion import Explosion
+from enemyflight import EnemyFlight, EnemyGroup
+from bullet import EnemyBullet
+
+class Decepticon:
+    def __init__(self, player, segment):
+        self.image = ENEMY_FLIGHT_IMAGE
+        self.original_image = self.image.copy()  # Keep the original image for rotation
+        self.rect = self.image.get_rect()
+        self.segment = segment  # Segment that the enemy is restricted to
+        self.player = player  # Store reference to the player
+        self.rect.x = random.randint(segment.left, segment.right - self.rect.width)
+        self.rect.y = 0  # Start at the top of the segment
+        self.speed = 3  # Speed of the enemy
+        self.health = 200  # Set initial health
+        self.shoot_timer = 0
+        self.shoot_interval = 60  # Time between shots in frames
+        self.bullets = []
+        self.explosion = None  # Explosion attribute
+        self.is_dead = False  # Flag to mark the enemy as dead
+
+
+    
