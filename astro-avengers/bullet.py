b94d5484e72da34dae5e58e88ecfca994a809383
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-14 21:24:42 +0530

    double bullet created

--- astro-avengers/bullet.py
+++ astro-avengers/bullet.py
@@ -29,3 +29,19 @@ class EnemyBullet:
     
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+
+
+class DoubleBullet:
+    def __init__(self, x, y):
+        self.image = DOUBLE_BULLET_IMAGE
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed = 10
+    
+    def update(self):
+        self.rect.y += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
