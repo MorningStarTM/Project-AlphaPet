5058af0fee28fba0bc3c4d2747c724afa4b2465a
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-22 21:38:53 +0530

    pet support functions created

--- astro-avengers/pet.py
+++ astro-avengers/pet.py
@@ -86,3 +86,11 @@ class Pet:
         blit_rotate_center(win, self.image, (self.x, self.y), self.angle)
         for bullet in self.bullets:
             bullet.draw(win)
+
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center
+    )
+    win.blit(rotated_image, new_rect.topleft)
