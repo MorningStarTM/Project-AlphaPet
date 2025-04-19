cbd56af33e05d67905d3d38fcb3e5da6bf7e8270
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-04-19 22:05:16 +0530

    updated

--- astro-avengers/explosion.py
+++ astro-avengers/explosion.py
@@ -2,17 +2,17 @@ import pygame
 from const import *
 
 class Explosion:
-    def __init__(self, x, y):
-        self.images = LIST_OF_EXPLOSION_IMAGE  # Load explosion images
+    def __init__(self, x, y, scale=1.0):
+        self.images = [pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                       for img in LIST_OF_EXPLOSION_IMAGE]
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(center=(x, y))
-        self.animation_speed = 5  # Adjust for how fast the explosion animates
+        self.animation_speed = 5
         self.counter = 0
         self.done = False
 
     def update(self):
-        """ Update the explosion animation """
         if not self.done:
             self.counter += 1
             if self.counter >= self.animation_speed:
@@ -22,8 +22,9 @@ class Explosion:
                     self.done = True
                 else:
                     self.image = self.images[self.index]
+                    self.rect = self.image.get_rect(center=self.rect.center)
 
-    def draw(self, screen):
-        """ Draw the explosion on the screen """
+    def draw(self, screen, offset=(0, 0)):
         if not self.done:
-            screen.blit(self.image, self.rect)
+            screen.blit(self.image, (self.rect.x + offset[0], self.rect.y + offset[1]))
+
